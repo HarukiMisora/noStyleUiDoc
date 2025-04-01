04a03b9da7aadaafb219610db2cb39a1b9411181
lilmonix3 <wwssjjhhaamm@qq.com>
2025-04-02 01:16:49 +0800

    fix：修改引入

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import nostyleui from 'nostyleui/index'
-
+import nostyleui from 'nostyleui'
+import 'nostyleui/style'  
 import router from './router'
 
 import {VMdPreview,VueMarkdownEditor} from '../lib/vMdMarker/vMdMarker.ts';
