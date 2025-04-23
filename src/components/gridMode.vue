c20258a74e75c01fda8cffdf5af3261980140716
lilmonix3 <wwssjjhhaamm@qq.com>
2025-04-23 15:01:52 +0800

    refactor：重构hover属性文档

---
+++ src/components/gridMode.vue
@@ -0,0 +1,34 @@
+
+
+<script lang="ts" setup>
+import { inject } from 'vue'
+
+const gridMode = inject<boolean>('gridMode')
+
+
+</script>
+
+<template>
+  <w-div grid="col--650 g-20">
+    <template v-if="!gridMode">
+        <w-div flex="g-20 col" :ghost="gridMode" v-if="$slots.default">
+        <template v-for="(child, index) in $slots.default()" :key="index">
+
+          <component :is="child" v-if="(index + 1) % 2 === 1"/>
+        </template>
+      </w-div>
+
+      <w-div flex="g-20 col" :ghost="gridMode" v-if="$slots.default">
+        <template v-for="(child, index) in $slots.default()" :key="index">
+          <component :is="child" v-if="(index + 1) % 2 === 0" />
+        </template> 
+      </w-div>
+
+    </template>
+    <slot v-else></slot>
+
+  </w-div>
+</template>
+
+
+<style></style>
--- src/docs/hoverDoc.vue
+++ src/docs/hoverDoc.vue
@@ -5,6 +5,7 @@ import codePreview from '../components/codePreview.vue';
 import { createCode } from '../components/createCode';
 import { as } from '../assets/assets';
 import PropsDisplay from '../components/propsDisplay.vue';
+import GridMode from '../components/gridMode.vue';
 
 const baseCode = createCode(`
 <w-div flex="g-10">
@@ -124,54 +125,48 @@ const  bdProps = ([
             <h1>hover 属性集</h1>
             <notice class="mt-10" title="作用" msg="定义盒子的鼠标 hover 样式;hover属性集一条参数的前缀是其它属性集的名称，比如你想实现hover更换背景色，应该是hover='bg-red'。<br/>而对子参数的前缀与值，比如你想实现hover改变图片的backgroundPosition属性,你可以参考bg属性，bg='x-50'，要在hover里面实现这个效果应该是hover='bg-x:10'<br/>多条子参数连接起来应该是hover='bg-red-x:50-w:500-fill'"></notice>
         </w-div>
-        <w-div :flex="['wrap','j-evenly']">
-            <w-div w="p48">
-                <w-div :flex="['col','g-10']">
-                    <codePreview  title="基础用法" >
-                        <w-div flex="g-10">
-                            <w-group flex="center"   >
-                                <w-div hover="c=red">red</w-div>
-                                <w-div hover="c=#0f0">#0f0</w-div>
-                                <w-div hover="c=rgb(0,0,255)">rgb(0,0,255)</w-div>
-                                <w-div hover="c=red+blue">red blue</w-div>
-                            </w-group>
-                        </w-div> 
-                        <w-div flex="g-10" pt="10">
-                            <w-group flex="center"  c="#fff" bg="red" >
-                                <w-div hover="c=black bg=green">背景颜色</w-div>
-                                <w-div hover="c=black bg=#0f0">背景颜色</w-div>
-                                <w-div hover="c=red bg=rgb(0,0,0)">背景颜色</w-div>
-                                <w-div hover="bg=rgba(0,0,0,0.5)-#fff">背景颜色</w-div>
-                            </w-group>
-                        </w-div>
-                        <w-div :bg="as.img+' size-50% center'" h="125" mt="20" hover="bg=size-100%" transition=".2s"></w-div>
-                    </codePreview>
+        <GridMode>
+            <codePreview  title="基础用法" >
+                <w-div flex="g-10">
+                    <w-group flex="center" >
+                        <w-div hover="c=red">red</w-div>
+                        <w-div hover="c=#0f0">#0f0</w-div>
+                        <w-div hover="c=rgb(0,0,255)">rgb(0,0,255)</w-div>
+                        <w-div hover="c=red+blue">red blue</w-div>
+                    </w-group>
+                </w-div> 
+                <w-div flex="g-10" pt="10">
+                    <w-group flex="center"  c="#fff" bg="red" >
+                        <w-div hover="c=black bg=green">背景颜色</w-div>
+                        <w-div hover="c=black bg=#0f0">背景颜色</w-div>
+                        <w-div hover="c=red bg=rgb(0,0,0)">背景颜色</w-div>
+                        <w-div hover="bg=rgba(0,0,0,0.5)-#fff">背景颜色</w-div>
+                    </w-group>
                 </w-div>
+                <w-div :bg="as.img+' size-50% center'" h="125" mt="20" hover="bg=size-100%" transition=".2s"></w-div>
+            </codePreview>
 
-            </w-div>
-            <w-div w="p48" :flex="['col','g-10']">
-                <codePreview  title="背景图片" >
-                    <w-div :flex="['g-10','wrap']" c="green">
-                        <w-group transition radius="p50" w="100" h="100" flex="center" :bg="[as.head,'fill']" :hover="`bg=${as.img};cover`" >
-                            <w-div bd="l-solid t-dashed r-double b-dotted" ></w-div>
-                            <w-div bd="dashed red x-solid"></w-div>
-                            <w-div bd="dotted blue y-red" hover="bg-none"></w-div>
-                            <w-div bd="double r-solid-red+#fff"></w-div>
-                        </w-group>
-                    </w-div>
-                    <w-div :flex="['g-10','wrap']">
-                        <w-group transition radius="p50" w="100" h="100" flex="center" :bg="[as.head,'fill','r-n']" hover="bg=cover;size-50">
-                            <w-div bd="t-groove" hover="bg=left"></w-div>
-                            <w-div bd="r-outset-red" hover="bg=right"></w-div>
-                            <w-div bd="b-inset blue" hover="bg=top;center"></w-div>
-                            <w-div bd="l-ridge" hover="bg=bottom;center"></w-div>
-                        </w-group>
-                    </w-div>
-                </codePreview>
-            </w-div>
-            
- 
-        </w-div>
+            <codePreview  title="背景图片" >
+                <w-div :flex="['g-10','wrap']" c="green">
+                    <w-group transition radius="p50" w="100" h="100" flex="center" :bg="[as.head,'fill']" :hover="`bg=${as.img};cover`" >
+                        <w-div bd="l-solid t-dashed r-double b-dotted" ></w-div>
+                        <w-div bd="dashed red x-solid"></w-div>
+                        <w-div bd="dotted blue y-red" hover="bg-none"></w-div>
+                        <w-div bd="double r-solid-red+#fff"></w-div>
+                    </w-group>
+                </w-div>
+                <w-div :flex="['g-10','wrap']">
+                    <w-group transition radius="p50" w="100" h="100" flex="center" :bg="[as.head,'fill','r-n']" hover="bg=cover;size-50">
+                        <w-div bd="t-groove" hover="bg=left"></w-div>
+                        <w-div bd="r-outset-red" hover="bg=right"></w-div>
+                        <w-div bd="b-inset blue" hover="bg=top;center"></w-div>
+                        <w-div bd="l-ridge" hover="bg=bottom;center"></w-div>
+                    </w-group>
+                </w-div>
+            </codePreview>
+
+        </GridMode>
+
 
         <PropsDisplay  :props="bdProps">
                 <w-h1 px="20" pt="20" >API</w-h1>
