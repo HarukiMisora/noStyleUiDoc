d0a92897f9561b091d5b165c9ea0f898b2a9af03
lilmonix3 <wwssjjhhaamm@qq.com>
2025-04-25 14:33:26 +0800

    fix：修改敬请期待页面

--- src/home/ComingSoon.vue
+++ src/home/ComingSoon.vue
@@ -9,10 +9,9 @@ import { as } from '../assets/assets';
     <w-div flex="col" h="$100%" bg="white" p="20">
         <w-div f="18" fw="600">个人开源，碎片化时间更新，白天要上班，午休要腾时间写web游戏引擎，下班了还要肝游戏，也就晚上睡前或者周末敲一敲，所以更新缓慢，请耐心等候。</w-div>
         <w-h1>
-            THE NEXT → + ts命令集(过渡动画)、hover拓展、hover命令盒子(暂未确定)、flex整改
+            THE NEXT → + 静态样式转到node环境解析和编译、WInput组件、position属性
         </w-h1>
         <w-div my="10" p="20" radius="12" :bg="[as.img,'cover','r-n']" flex="1"></w-div>
-
     </w-div>
 </template>
 
