412f06b2f0f3de9928ace998832f5f9954ca3912
lilmonix3 <wwssjjhhaamm@qq.com>
2025-08-15 13:34:27 +0800

    fix：修改敬请期待页面

--- src/brief/zcomingSoonDoc.vue
+++ src/brief/zcomingSoonDoc.vue
@@ -4,18 +4,17 @@
  * @desc 敬请期待
  * @groupName ...
  */ 
-import { as } from '../assets/assets';
+// import { as } from '../assets/assets';
 
 
 </script>
 
 <template>
     <w-div flex="col" h="100%" bg="white" p="20">
-        <w-div f="18" fw="600">个人开源，碎片化时间更新，白天要上班，午休要腾时间写web游戏引擎，下班了还要肝游戏，也就晚上睡前或者周末敲一敲，所以更新缓慢，请耐心等候。</w-div>
         <w-h1>
-            THE NEXT → + 静态样式转到node环境解析和编译、WInput组件、position属性
+            THE NEXT PLAN → 丰富更多的props  
         </w-h1>
-        <w-div my="10" p="20" radius="12" :bg="[as.img,'cover','r-n']" flex="1"></w-div>
+        <iframe my="10" p="5" radius="12" src="https://www.iamwzc.com/TMXK/ShootingBall/" flex="1"></iframe>
     </w-div>
 </template>
 
